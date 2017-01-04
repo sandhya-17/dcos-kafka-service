7afcf7ea58c7324e55e11ff34018f1f3739c6ae8
Michael Gummelt <mgummelt@mesosphere.io>
2017-01-04 12:18:40 -0800

    check length of list, not dict

--- integration/tests/test_upgrade.py
+++ integration/tests/test_upgrade.py
@@ -33,8 +33,8 @@ def teardown_module(module):
 def test_upgrade_downgrade():
     # Ensure both Universe and the test repo exist.
     if len(shakedown.get_package_repos()['repositories']) != 2:
-          print('No kafka test repo found.  Skipping test_upgrade_downgrade')
-          return
+        print('No kafka test repo found.  Skipping test_upgrade_downgrade')
+        return
 
     test_repo_name, test_repo_url = get_test_repo_info()
     test_version = get_pkg_version()
