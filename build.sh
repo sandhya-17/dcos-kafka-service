c318b75e11cfc3cc456597940bc3b86ef65eaa2e
sandhya-17 <sandhya.km91@yahoo.com>
2017-01-16 14:15:40 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -43,7 +43,7 @@ cd $REPO_ROOT_DIR
 
 _notify_github success "Build succeeded"
 
-./dcos-commons-tools/publish_aws.py \
+./dcos-commons-tools/publish_http.py \
   kafka \
   universe/ \
   kafka-scheduler/build/distributions/*.zip \
